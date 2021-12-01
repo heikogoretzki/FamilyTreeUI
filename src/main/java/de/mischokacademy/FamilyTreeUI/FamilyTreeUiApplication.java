927aac4ad389e5ec62850522fcab580c2f574300
heikogoretzki <heikogoretzki@gmail.com>
2021-12-01 13:02:35 +0100

    initial upload

---
+++ src/main/java/de/mischokacademy/FamilyTreeUI/FamilyTreeUiApplication.java
@@ -0,0 +1,13 @@
+package de.mischokacademy.FamilyTreeUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTreeUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTreeUiApplication.class, args);
+	}
+
+}
